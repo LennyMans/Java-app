ecac1fe413c9fb42807f32c758675118b40f77cf
FastNugget <julien.foliot@gmail.com>
2021-01-20 20:59:24 +0100

    end session4

---
+++ src/main/java/com/formation/tp/test/UserBean.java
@@ -0,0 +1,16 @@
+package com.formation.tp.test;
+
+
+public class UserBean {
+
+    private String ref_String_Name;
+    private String ref_String_FirstName;
+
+    public UserBean(String ref_String_Name, String ref_String_FirstName) {
+
+        this.ref_String_Name = ref_String_Name;
+        this.ref_String_FirstName = ref_String_FirstName;
+
+    }
+
+}
