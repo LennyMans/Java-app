25c48d73475e142062fd00d9674b6c5294002079
FastNugget <julien.foliot@gmail.com>
2021-01-20 20:33:15 +0100

    save..

--- src/main/java/com/formation/tp/keyboard/Keyboard_Context.java
+++ src/main/java/com/formation/tp/keyboard/Keyboard_Context.java
@@ -17,6 +17,7 @@ public class Keyboard_Context {
 
     // -- VARS
     final private static Object ref_Object_Lock_ArrayDeque_Input = new Object();
+    final static private int ref_Int_MaxSize_ArrayDeque = 5;
     private final ArrayDeque <String> ref_ArrayDeque_KeyboardEntry = new ArrayDeque<>();
     private final ThreadEngine_KeyboardContext ref_ThreadEngine_KeyboardContext = new ThreadEngine_KeyboardContext();
 
@@ -41,7 +42,8 @@ public class Keyboard_Context {
                 = Pattern.compile("^CustomerFirstName=([A-Z]{1}[a-z]{1,20}),CustomerName=([A-Z]{1}[a-z]{1,20})$");
 
     private static String ref_String_Response_Ok = "Ack receive";
-    private static String ref_String_Response_Nok = "Ack error";
+    private static String ref_String_Response_Nok_WrongSentence = "Ack error, wrong sentence";
+    private static String ref_String_Response_Nok_Busy = "Ack error - To busy, please retry again";
 
 
 
@@ -166,7 +168,11 @@ public class Keyboard_Context {
 
     // -- UTILITARY ----------------------------------------------------------------------
 
+    private boolean sizeStack_NotReach(){
 
+        return Keyboard_Context.this.ref_ArrayDeque_KeyboardEntry.size() < Keyboard_Context.ref_Int_MaxSize_ArrayDeque;
+
+    };
 
     public void sleepMyFriend(int ref_Int_TimeToSleep){
 
@@ -218,29 +224,68 @@ public class Keyboard_Context {
                  String ref_String_Read = Keyboard_Context.this.readInputStream();
 
                  // -- Check
-                 if(ref_String_Read == null) {
+                 if(ref_String_Read == null) { continue; }
 
-                     // -- Continue
-                     continue;
+
+                 // -- Build response
+                 String ref_String_ResponseToCommit = Keyboard_Context.ref_String_Response_Ok;
+
+                 String [] ref_Array_String_Response_Error_ToCommit
+                        = { ref_String_Response_Nok_WrongSentence
+                            , ref_String_Response_Nok_Busy};
+
+
+                 // -- Boolean for verif
+                 boolean ref_Boolean_Global_Verification = Boolean.TRUE;
+
+                 int ref_Int_Offset_SentenceVerification = 0;
+                 int ref_Int_Offset_BusyVerification = 1;
+
+                 boolean [] ref_Array_Boolean_IsOk = new boolean[2];
+                     ref_Array_Boolean_IsOk[ref_Int_Offset_SentenceVerification] = (Keyboard_Context.this.verifyInput(ref_String_Read));
+                     ref_Array_Boolean_IsOk[ref_Int_Offset_BusyVerification] = (Keyboard_Context.this.sizeStack_NotReach());
+
+                 // -- Set global
+                 for(boolean ref_Boolean_Unit : ref_Array_Boolean_IsOk){
+
+                     ref_Boolean_Global_Verification &= ref_Boolean_Unit;
 
                  }
 
-                 // -- Log
-              //   System.err.println("ThreadEngine_KeyboardContext - Sentence Receive:".concat(ref_String_Read));
 
-                 // -- Check & Commit
-                 if(Keyboard_Context.this.verifyInput(ref_String_Read) == Boolean.TRUE){
+                 // -- Check & commit
+                 if(ref_Boolean_Global_Verification == Boolean.FALSE){
+
+                    // -- Loop over test
+                   for(int ref_Int_Offset_A = 0; ref_Int_Offset_A < ref_Array_Boolean_IsOk.length; ref_Int_Offset_A++){
+
+                       // -- Check
+                       if(ref_Array_Boolean_IsOk[ref_Int_Offset_A] != Boolean.TRUE){
+
+                           // -- Set
+                           ref_String_ResponseToCommit = ref_Array_String_Response_Error_ToCommit[ref_Int_Offset_A];
+
+                           // -- Stop
+                           break;
+
+                       }
+
+                   }
+
+                 }
+
+
+
+                 // -- Commit response to remote end
+                 Keyboard_Context.this.writeOutputStream(ref_String_ResponseToCommit);
+
+
+
+                 // -- Check & add stack
+                 if(ref_String_ResponseToCommit.equals(Keyboard_Context.ref_String_Response_Ok)){
 
                      // -- Commit
                      Keyboard_Context.this.add_UserInput(ref_String_Read);
-
-                     // -- Commit response to remote end
-                     Keyboard_Context.this.writeOutputStream(ref_String_Response_Ok);
-
-                 }else{
-
-                     // -- Commit response to remote end
-                     Keyboard_Context.this.writeOutputStream(ref_String_Response_Nok);
 
                  }
 
