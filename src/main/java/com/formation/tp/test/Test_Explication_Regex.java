885d7dc945557993404a77a9979b4350f5bef427
Lenny Mans <mans.lenny@gmail.com>
2021-02-03 20:35:56 +0100

    session - 12 - hardword

--- src/main/java/com/formation/tp/test/Test_Explication_Regex.java
+++ src/main/java/com/formation/tp/test/Test_Explication_Regex.java
@@ -9,7 +9,7 @@ public class Test_Explication_Regex {
 
     public static void main (String [] ref_Array_String_Arg){
 
-     test1();
+     test2();
 
 
 
@@ -18,7 +18,7 @@ public class Test_Explication_Regex {
     public static void test1(){
 
         // -- INIT DATA
-        String ref_input = "WINNER-JesSuisUneLibellule-fWINNER_ViveLensWINNER";
+        String ref_input = "WINNER-JesSuisUneLibellule-fWINNER_ViveLensWINNER54654654";
         String ref_String_Regex ="WINNER";
         StringBuilder ref_StringBuilder = new StringBuilder();
 
@@ -46,23 +46,26 @@ public class Test_Explication_Regex {
         // -- DEDUCE POSITION TO COPY
         for(int ref_Int_A = 0; ref_Int_A < ref_ArrayList_PositionToRemove.size(); ref_Int_A++){
 
+            // -- RETRIEVE
             int [] ref_Array_ToRemoveUnit = ref_ArrayList_PositionToRemove.get(ref_Int_A);
 
+            // -- INIT
             int[] ref_Array_Unit = new int[2];
 
-            // -- CASE DEBUT
+
+            // -- CASE PREMIER MATCH
             if(ref_Int_A == 0){
 
                 ref_Array_Unit[0] = ref_Array_ToRemoveUnit[0] == 0 ? ref_Array_ToRemoveUnit[1] : 0;
                 ref_Array_Unit[1] = ref_ArrayList_PositionToRemove.get(ref_Int_A+1)[0];
 
-            // -- CASE FIN
+            // -- CASE DERNIER MATCH
             }else if (ref_Int_A == ref_ArrayList_PositionToRemove.size() -1){
 
-                ref_Array_Unit[0] =  ref_Array_ToRemoveUnit[1];
+                ref_Array_Unit[0] = ref_Array_ToRemoveUnit[1];
                 ref_Array_Unit[1] = ref_input.length();
 
-            // -- CASE MILIEU
+            // -- CASE MATCH ENTRE DEBUT ET FIN DE MATCH
             }else{
 
                 ref_Array_Unit[0] =  ref_Array_ToRemoveUnit[1];
@@ -90,8 +93,60 @@ public class Test_Explication_Regex {
 
     }
 
-    public static void test2(){}
+    public static void test2(){
 
-    public static void test3(){}
+
+       // -- INIT DATA
+        String ref_input = "WINNER-JesSuisUneLibellule-fWINNER_ViveLensWINNER54654654";
+        String ref_String_Regex ="WINNER";
+        StringBuilder ref_StringBuilder = new StringBuilder();
+
+        // -- INIT REGEX
+        Pattern ref_Pattern = Pattern.compile(ref_String_Regex);
+        Matcher ref_Matcher;
+
+        // -- APPLY REGEX AND EXTRACT
+        ref_StringBuilder.append(ref_input);
+
+        // -- WORk
+        while(true){
+
+            // -- Start match
+            ref_Matcher = ref_Pattern.matcher(ref_StringBuilder.toString());
+
+            // -- Check
+            if(ref_Matcher.find() == Boolean.FALSE){
+
+                // -- out of loop
+                break;
+
+            }
+
+            // --  Set
+            ref_StringBuilder.replace( ref_Matcher.start(), ref_Matcher.end(), "");
+
+        }
+
+        // -- COMMIT
+        System.out.println("STRING REDUCED=" + ref_StringBuilder.toString());
+
+
+    }
+
+    public static void test3(){
+
+        // -- INIT DATA
+        String ref_input ="WINNER";
+        String ref_String_Regex = "^(WINNER)$";
+
+
+        // -- INIT REGEX
+        Pattern ref_Pattern = Pattern.compile(ref_String_Regex);
+        Matcher ref_Matcher =  ref_Pattern.matcher(ref_input);
+
+
+
+
+    }
 
 }
