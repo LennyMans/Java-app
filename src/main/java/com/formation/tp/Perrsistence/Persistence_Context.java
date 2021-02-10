280ace495b6863abe04fb38915b025a51be44218
Lenny Mans <mans.lenny@gmail.com>
2021-02-10 21:08:10 +0100

    Session 15 : settup persistence service

--- src/main/java/com/formation/tp/Perrsistence/Persistence_Context.java
+++ src/main/java/com/formation/tp/Perrsistence/Persistence_Context.java
@@ -3,6 +3,8 @@ package com.formation.tp.Perrsistence;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.concurrent.Executors;
 
 public class Persistence_Context {
@@ -10,7 +12,7 @@ public class Persistence_Context {
 
     // -- CALL BACK -------------------------------------------------------
 
-    private void persistInFile(StringBuffer ref_String_Buffer){
+    public void persistInFile(StringBuffer ref_String_Buffer){
 
         // -- Init
         Runnable ref_Runnable = new Runnable() {
@@ -53,5 +55,98 @@ public class Persistence_Context {
 
     }
 
+    public static void insert(String ref_String_DateToInsert){
+
+        // -- CONS
+        final String ref_String_Skeleton_Query = "INSERT INTO google_date2 (date_retrieve) VALUES ('%s')";
+        String ref_String_Builder_Query = String.format( ref_String_Skeleton_Query,  ref_String_DateToInsert);
+
+
+        // -- VARS
+        Connection ref_Connexion = null;
+        Statement ref_Statement = null;
+        ResultSet resultSet = null;
+        ArrayList<String> arrayListUnitQueryResponse = null;
+
+        // -- Work
+
+        try {
+
+            // -- Init
+            ref_Connexion = DriverManager.getConnection("jdbc:mariadb://localhost:3306/google_date?user=root&password=lenny1234");
+            ref_Statement = ref_Connexion.createStatement();
+            resultSet = ref_Statement.executeQuery(ref_String_Builder_Query);
+
+            // -- Close connection
+            ref_Connexion.close();
+
+
+        } catch (SQLException ref_SQLException_a) {
+            ref_SQLException_a.printStackTrace();
+            try {
+
+                if (ref_Connexion != null) { ref_Connexion.close(); }
+
+            } catch (SQLException ref_SQLException_b) {ref_SQLException_b.printStackTrace(); }
+        }
+
+    }
+
+    public static ArrayList<String> select(){
+
+        // -- CONS
+        final String designedQuery = "select * from google_date2";
+
+        // -- VARS
+        Connection ref_Connexion = null;
+        Statement ref_Statement = null;
+        ResultSet resultSet = null;
+        ArrayList<String> arrayListUnitQueryResponse = new ArrayList<>();
+
+        // -- Work
+        try {
+
+            // -- Init
+            ref_Connexion = DriverManager.getConnection("jdbc:mariadb://localhost:3306/google_date?user=root&password=lenny1234");
+            ref_Statement = ref_Connexion.createStatement();
+            resultSet = ref_Statement.executeQuery(designedQuery);
+
+
+            // -- Work
+            while (resultSet.next()) {
+
+                // -- Retrieve
+                String ref_String_Date = resultSet.getString("date_retrieve");
+
+                // -- Commit
+                arrayListUnitQueryResponse.add(ref_String_Date);
+
+            }
+
+            // -- Close connection
+            ref_Connexion.close();
+
+
+        } catch (SQLException ref_SQLException_a) {
+
+            // -- First log
+            ref_SQLException_a.printStackTrace();
+
+            try {
+
+                if (ref_Connexion != null) { ref_Connexion.close(); }
+
+            } catch (SQLException ref_SQLException_b) {
+
+                // -- Second log
+                ref_SQLException_b.printStackTrace();
+            }
+        }
+
+        // -- Commit
+        return arrayListUnitQueryResponse;
+
+
+    }
 
 }
--- src/main/java/com/formation/tp/test/TestDb.java
+++ src/main/java/com/formation/tp/test/TestDb.java
@@ -9,6 +9,50 @@ public class TestDb {
     
     public static void main (String [] args) {
 
+        example_insert("TEST INSERT Lenny");
+        example_select();
+
+    }
+
+    public static void example_insert(String ref_String_DateToInsert){
+
+        // -- CONS
+        final String ref_String_Skeleton_Query = "INSERT INTO google_date2 (date_retrieve) VALUES ('%s')";
+        String ref_String_Builder_Query = String.format( ref_String_Skeleton_Query,  ref_String_DateToInsert);
+
+
+        // -- VARS
+        Connection ref_Connexion = null;
+        Statement ref_Statement = null;
+        ResultSet resultSet = null;
+        ArrayList<String> arrayListUnitQueryResponse = null;
+
+        // -- Work
+
+        try {
+
+            // -- Init
+            ref_Connexion = DriverManager.getConnection("jdbc:mariadb://localhost:3306/google_date?user=root&password=lenny1234");
+            ref_Statement = ref_Connexion.createStatement();
+            resultSet = ref_Statement.executeQuery(ref_String_Builder_Query);
+
+            // -- Close connection
+            ref_Connexion.close();
+
+
+        } catch (SQLException ref_SQLException_a) {
+            ref_SQLException_a.printStackTrace();
+            try {
+
+                if (ref_Connexion != null) { ref_Connexion.close(); }
+
+            } catch (SQLException ref_SQLException_b) {ref_SQLException_b.printStackTrace(); }
+        }
+
+    }
+
+    public static void example_select(){
+
         // -- CONS
         final String designedQuery = "select * from google_date2";
 
@@ -59,6 +103,7 @@ public class TestDb {
 
             } catch (SQLException ref_SQLException_b) {ref_SQLException_b.printStackTrace(); }
         }
-
     }
+
+
 }
